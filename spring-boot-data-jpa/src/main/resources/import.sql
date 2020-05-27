aa99e2113d1d68b537353d92fdd7c1d65f604998
Raúl Mauricio Álvarez Espinosa <47632632+mares-004@users.noreply.github.com>
2020-05-27 16:06:14 -0500

    Add files via upload

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido,email,create_at) values (1, 'morris', 'mauricio', 'morris@mum.edu', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido,email,create_at) values (2, 'clara', 'lopez', 'clarin@mum.edu', '2017-08-28');
