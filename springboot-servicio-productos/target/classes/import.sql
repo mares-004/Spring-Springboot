364831d6d0f96284adfbe63fb392c47097be29d4
Raúl Mauricio Álvarez Espinosa <47632632+mares-004@users.noreply.github.com>
2020-05-27 16:02:16 -0500

    Add files via upload

---
+++ springboot-servicio-productos/target/classes/import.sql
@@ -0,0 +1,5 @@
+insert into productos (nombre, precio, create_at) values ('reebok', 200,  now());
+insert into productos (nombre, precio, create_at) values ('adidas', 167,  now());
+insert into productos (nombre, precio, create_at) values ('nike', 180,  now());
+insert into productos (nombre, precio, create_at) values ('accis', 150,  now());
+insert into productos (nombre, precio, create_at) values ('Skeacher', 100,  now());
